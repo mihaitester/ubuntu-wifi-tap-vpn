f1a45146c8a9d3ef27b3711581b636b02c3bf981
Mihai Moldovan <mihaiflorinmoldovan@yahoo.com>
2025-04-26 05:44:29 +0000

    (wireshark): added timestamp to logfile, so there are no overwrites

--- wireshark_ethernet.cmd
+++ wireshark_ethernet.cmd
@@ -1,2 +1,8 @@
-"C:\Program Files\Wireshark\wireshark.exe" -k -i Ethernet -b duration:1800 -w wireshark_ethernet.capture --log-file wireshark_ethernet.log
+set starttime=%date%_%time%
+set "starttime=%starttime:/=-%"
+set "starttime=%starttime::=-%"
+set "starttime=%starttime: =_%"
+echo %starttime%
+
+"C:\Program Files\Wireshark\wireshark.exe" -k -i Ethernet -b duration:1800 -w wireshark_ethernet.capture --log-file wireshark_ethernet_%starttime%.log
 pause
--- wireshark_wifi.cmd
+++ wireshark_wifi.cmd
@@ -1,2 +1,8 @@
-"C:\Program Files\Wireshark\wireshark.exe" -k -i WiFi -b duration:1800 -w wireshark_wifi.capture --log-file wireshark_wifi.log
+set starttime=%date%_%time%
+set "starttime=%starttime:/=-%"
+set "starttime=%starttime::=-%"
+set "starttime=%starttime: =_%"
+echo %starttime%
+
+"C:\Program Files\Wireshark\wireshark.exe" -k -i WiFi -b duration:1800 -w wireshark_wifi.capture --log-file wireshark_wifi_%starttime%.log
 pause
