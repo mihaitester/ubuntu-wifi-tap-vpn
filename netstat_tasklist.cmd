bba4a0e5d4b2420a523ccbf720abafe194fe4e21
Mihai Moldovan <mihaiflorinmoldovan@yahoo.com>
2025-04-27 16:09:49 +0000

    (docs): added new observations

--- netstat_tasklist.cmd
+++ netstat_tasklist.cmd
@@ -18,13 +18,13 @@ set datetime=%date%_%time%
 set "datetime=%datetime:/=-%"
 set "datetime=%datetime::=-%"
 set "datetime=%datetime: =_%"
-rem why does this spam files, makes me think not just of rootkitted OS, but of fucking fake VM os running
+rem why does this spam files, makes me think not just of rootkitted OS, but of fake VM os running
 rem so having %datetime% instead of %date_time% fixes the issue, means CMD parser is broken
 
 echo %datetime% >> "netstat_%starttime%.txt"
 netstat.exe -nao >> "netstat_%starttime%.txt"
 rem this requires administrator priviledges
-rem netstat -nao >> "netstat_%starttime%.txt"
+rem netstat -naob >> "netstat_%starttime%.txt"
 echo %datetime% >> "tasklist_%starttime%.txt"
 tasklist.exe >> "tasklist_%starttime%.txt"
 
@@ -36,7 +36,7 @@ cls
 rem now we need to keep printing this in a loop, so that it can be correlated with wireshark logs
 rem [ https://stackoverflow.com/questions/17584282/how-to-check-if-a-parameter-or-variable-is-a-number-in-a-batch-script ] => handling numerical variables
 if %loopcount% GTR 0 (
-	set /a loopcount=%loopcount%-1
+	set /a loopcount=%loopcount% - 1
 	goto :loop
 ) else (
 	echo Finished running script
