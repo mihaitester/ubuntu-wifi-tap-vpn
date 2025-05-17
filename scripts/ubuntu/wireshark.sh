127bb467182b63fee3174c47cc15a8e68b4f5bad
Mihai Moldovan <mihaiflorinmoldovan@yahoo.com>
2025-05-17 16:41:12 +0000

    (scripts): added more interfaces to capture

--- scripts/ubuntu/wireshark.sh
+++ scripts/ubuntu/wireshark.sh
@@ -6,8 +6,6 @@ fi
 echo duration=${duration}
 
 bash -c ./eth0.sh $duration &
-bash -c ./eth1.sh $duration &
-bash -c ./br0.sh $duration &
 
 # note: observed weird packets on loopback interface before getting hacked
 bash -c ./lo.sh $duration &
