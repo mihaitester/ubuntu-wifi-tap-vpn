083318b6531b34c7d76f2b05aab733fe1ee6032f
Mihai Moldovan <mihaiflorinmoldovan@yahoo.com>
2025-05-18 14:47:52 +0000

    (docs): added more observations, it is super frustrating

--- scripts/windows/explorer.cmd
+++ scripts/windows/explorer.cmd
@@ -1,11 +1,13 @@
-taskkill /f /im chrome.exe
+taskkill /f /im explorer.exe
 
 rem note: for affinity setting to work, need to enable all processors [number_of_processors=8] in msconfig.msc [ https://www.reddit.com/r/Windows10/comments/qys8ob/any_idea_if_changing_the_number_of_processors/ ]
 rem note: for affinity setting to work, need to have user freshly logged into session, otherwise running explorer.exe under affinity settings will block child processes from accessing different procesors than parent
 rem note: affinity mask is basically the bitmask in hex, use calculator in programmer mode, with binary view to get the mask easier
 
-cd C:\Users\%USERNAME%\AppData\Local\Google\Chrome\Application
-rem start /w /low /affinity 0xF0 chrome.exe
-rem start /w /low chrome.exe
-start /w /affinity 0xF0 chrome.exe
-pause
+rem observation: doing this blocks the desktop viewer and the taskbar
+rem start /w /low /affinity 0x2 explorer.exe "C:\users\%USERNAME%\Desktop"
+
+rem note: all child processes or everything else that user starts afterwards will have this bitmask applied
+rem observation: so doing this restarts explorer and desktop viewer
+start /w /low /affinity 0x2 explorer.exe
+rem explorer.exe "C:\users\%USERNAME%\Desktop"
--- scripts/windows/firefox.cmd
+++ scripts/windows/firefox.cmd
@@ -1,6 +1,10 @@
 taskkill /f /im firefox.exe
 cd "C:\Users\%USERNAME%\AppData\Local\Mozilla Firefox"
 
+rem note: for affinity setting to work, need to enable all processors [number_of_processors=8] in msconfig.msc [ https://www.reddit.com/r/Windows10/comments/qys8ob/any_idea_if_changing_the_number_of_processors/ ]
+rem note: for affinity setting to work, need to have user freshly logged into session, otherwise running explorer.exe under affinity settings will block child processes from accessing different procesors than parent
+rem note: affinity mask is basically the bitmask in hex, use calculator in programmer mode, with binary view to get the mask easier
+
 rem start "firefox-parent-process-exploit-blocker" /w /affinity 0xC /d "C:\Users\%USERNAME%\AppData\Local\Mozilla Firefox" firefox.exe
 rem start "firefox-parent-process-exploit-blocker" /w /affinity 0xC firefox.exe
 rem echo Firefox closed its parent process !!!
@@ -10,5 +14,5 @@ rem this does not work, hence need powershell holder
 rem start "firefox-parent-process-exploit-blocker" /w /affinity 0xC cmd.exe /k "cmd.exe /k firefox.exe "
 rem powershell "Start-Process -Wait -FilePath 'C:\Users\%USERNAME%\AppData\Local\Mozilla Firefox\firefox.exe'"
 
-start "firefox-parent-process-exploit-blocker" /w /affinity 0xC cmd.exe /c "powershell "Start-Process -Wait -FilePath 'C:\Users\%USERNAME%\AppData\Local\Mozilla Firefox\firefox.exe'" "
+start "firefox-parent-process-exploit-blocker" /w /affinity 0xC cmd.exe /c "powershell.exe \"Start-Process -Wait -FilePath 'C:\Users\%USERNAME%\AppData\Local\Mozilla Firefox\firefox.exe'\" "
 pause
